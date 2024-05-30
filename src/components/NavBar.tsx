f1b2684946a06a9cf0f30bc388fe7856fa45a1a9
Lukas Somwong <93167066+ItsJustLukas78@users.noreply.github.com>
2024-05-30 00:46:02 -0700

    better navbar experience
    Signed-off-by: Lukas Somwong <93167066+ItsJustLukas78@users.noreply.github.com>

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,12 +8,13 @@ import Link from "next/link";
 import { IoIosArrowDown } from "react-icons/io";
 import {TeamType, Teams} from "@/data/teams";
 import Hamburger from 'hamburger-react'
-import { useState } from "react";
+import {useEffect, useState} from "react";
+import {useParams, usePathname, useRouter} from "next/navigation";
 
 
 
 const navBar = cva(["flex", "items-center", "justify-center", "h-24",
-  "w-full", "relative", "top-0", "left-0", "", "z-10", "pr-4", "bg-gradient-to-b", "from-black/80", "to-black/1"], {
+  "w-full", "sticky", "top-0", "left-0", "", "z-10", "pr-4"], {
   variants: {
     intent: {
       primary: ["text-primary"],
@@ -29,18 +30,27 @@ export interface NavBarProps extends VariantProps<typeof navBar> {
 }
 
 export function NavBar({ intent, className }: NavBarProps) {
+  const pathname = usePathname()
+  const params = useParams()
+
   const [hamburgerOpen, setHamburgerOpen] = useState(false)
   const [mobileTeamsExpanded, setMobileTeamsExpanded] = useState(false)
   const [mobileTimelinesExpanded, setMobileTimelinesExpanded] = useState(false)
 
-  return <div className={twMerge(navBar({ intent, className }))}>
+  useEffect(() => {
+    setHamburgerOpen(false)
+    setMobileTeamsExpanded(false)
+    setMobileTimelinesExpanded(false)
+  }, [pathname, params])
+
+  return <div className={twMerge(navBar({ intent, className })) + (hamburgerOpen ? " bg-[var(--primary-background-color)]" : " bg-gradient-to-b from-black/80 to-black/1")}>
     <div className="max-w-screen-xl 2xl:mx-4 ml-8 mr-4 flex w-full">
       <Link className="font-extrabold flex items-center gap-2 tracking-wide sm:text-lg text-sm" href="/">
         <Image src="/GFRLogo.png" alt="GFR Logo" width={50} height={50} className="hidden sm:block" />
         <Image src="/GFRLogo.png" alt="GFR Logo" width={30} height={30} className="block sm:hidden" />
         <span className="from-[#DB4A4A] via-[#8FB5E7] to-white bg-clip-text text-transparent bg-gradient-to-r">Gael Force Robotics</span>
       </Link>
-      <div className="lg:hidden ml-auto">
+      <div className="lg:hidden ml-auto text-primary">
         <Hamburger toggled={hamburgerOpen} toggle={setHamburgerOpen} direction="left" size={28} />
       </div>
       <div  className="hidden justify-center space-x-8 lg:flex ml-auto sm:text-lg text-sm [&>*]:font-medium [&>*]:h-min [&>*]:transition-colors [&>*]:duration-500">
@@ -77,7 +87,7 @@ export function NavBar({ intent, className }: NavBarProps) {
         <Link href="/#events" className="hover:text-[#DB4A4A]">Events</Link>
         <Link href="/#about" className="hover:text-[#DB4A4A]">About Us</Link>
         <div className="relative flex flex-col items-start">
-          <button className="hover:text-[#DB4A4A]">Teams <IoIosArrowDown className="inline" onClick={() => setMobileTeamsExpanded(!mobileTeamsExpanded)} /></button>
+          <Link className="hover:text-[#DB4A4A]" href="/#teams">Teams <IoIosArrowDown className="inline" onClick={() => setMobileTeamsExpanded(!mobileTeamsExpanded)} /></Link>
           <div className={`${mobileTeamsExpanded ? "max-h-80 visible mt-2" : "max-h-0 invisible"} overflow-hidden flex flex-col gap-3`}>
             {Teams.map((team: TeamType) => {
               return <Link key={team.teamNumber} href={`/team/${team.teamNumber}`} className="hover:text-[#DB4A4A] ml-4">{team.teamNumber}</Link>
@@ -86,7 +96,7 @@ export function NavBar({ intent, className }: NavBarProps) {
         </div>
         <Link href="/join" className="hover:text-[#DB4A4A]">Join Us</Link>
         <div className="relative group">
-          <button className="hover:text-[#DB4A4A]">Timelines <IoIosArrowDown className="inline" onClick={() => setMobileTimelinesExpanded(!mobileTimelinesExpanded)} /></button>
+          <button className="hover:text-[#DB4A4A]" onClick={() => setMobileTimelinesExpanded(!mobileTimelinesExpanded)}>Timelines <IoIosArrowDown className="inline" /></button>
           <div className={`${mobileTimelinesExpanded ? "max-h-80 visible mt-2" : "max-h-0 invisible"} overflow-hidden flex flex-col gap-3`}>
             <Link href="/timeline/photos" className="hover:text-[#DB4A4A] ml-4">Photos</Link>
             <Link href="/timeline/officers" className="hover:text-[#DB4A4A] ml-4">Officers</Link>
