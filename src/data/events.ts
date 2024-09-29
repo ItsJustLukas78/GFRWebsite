d159d274925bca7d68e6be6d22493b6abcd40dc9
Lukas Somwong <93167066+ItsJustLukas78@users.noreply.github.com>
2024-09-29 14:23:27 -0700

    workshop flier
    Signed-off-by: Lukas Somwong <93167066+ItsJustLukas78@users.noreply.github.com>

--- src/data/events.ts
+++ src/data/events.ts
@@ -35,6 +35,7 @@ export const events: EventType[] = [
     description: "Gael Force Robotics' annual VEX Robotics Competition that brings teams from all over the Bay Area!",
     image: "/botb2024.jpg",
     size: "twoxoney",
+    link: "https://drive.google.com/file/d/14lFOpctJCyvZNEiXj2ISfpNsxsyBSJ6p/view?usp=sharing"
   },
   {
     title: "All-Inclusive Workshop",
